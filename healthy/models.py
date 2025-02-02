2a04790bb830ac6e76eb513cdbcf4f02fb6ee9b0
zakis <zakisgm@gmail.com>
2025-02-02 03:34:59 +0200

    start 2

--- healthy/models.py
+++ healthy/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+class Products(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    image = models.ImageField(null=True)
+    category = models.ForeignKey('Categories', null=True, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey('Manufacturers', null=True, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+
+class Categories(models.Model):
+    title_c= models.CharField(max_length=100)
+    description_c = models.TextField()
+    image_c = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.title_c
+
+class Manufacturers(models.Model):
+    title_m = models.CharField(max_length=100)
+    description_m = models.TextField()
+    image_m = models.ImageField(null=True)
+
+    def __str__(self):
+        return self.title_m
+
--- healthy/views.py
+++ healthy/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.template.defaulttags import comment
+
+from .models import  Products,Categories,Manufacturers
+from django.utils import timezone
 
 # Create your views here.
+def post_list(request):
+    cat = request.GET.get('cat')
+
+    if cat is None:
+       products = Products.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    elif cat.isdigit():
+        cat = int(cat)
+        products = Products.objects.filter(published_date__lte=timezone.now()).filter(category=cat).order_by('published_date')
+    else:
+        products = Products.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {'products': products})
+
+
+
+def post_detail(request, pk):
+    products_detail = get_object_or_404(Products, pk=pk)
+    return render(request, 'blog/post_detail.html', {'products_detail': products_detail})
