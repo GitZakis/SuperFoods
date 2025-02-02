3ea938698d1762038bd050fa82a7d89cce4dcbad
zakis <zakisgm@gmail.com>
2025-02-02 14:37:36 +0200

    sunday 3 commit

--- SuperFoods/urls.py
+++ SuperFoods/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('healthy.urls')),
